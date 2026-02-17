538a08c70fe6e4ca92c87775ba18756744460e7b
Amirhossein Bakhshayeshiyan <bamirhossein65@gmail.com>
2026-02-17 12:46:55 +0330

    get request in fast api

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+result = []
+
+books = [
+    {"title": "Clean Code", "author": "Robert C. Martin", "publisher": "Prentice Hall"},
+    {"title": "Atomic Habits", "author": "James Clear", "publisher": "Avery"},
+    {"title": "Deep Work", "author": "Cal Newport", "publisher": "Grand Central"},
+    {"title": "The Pragmatic Programmer", "author": "Andrew Hunt", "publisher": "Addison-Wesley"}
+]
+
+@app.get("/search_books/{key}")
+def search_books(key:str):
+    for i in books:
+        if(
+            key.lower() in i["title"].lower()
+            or key.lower() in i["author"].lower()
+            or key.lower() in i["publisher"].lower()
+        ):
+            
+         return i
